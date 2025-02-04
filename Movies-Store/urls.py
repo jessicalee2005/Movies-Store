af306d2f639c93ab2cdedb6ec4e6c42a90bf0cdc
Maddie White <69733729+MuffinHead12@users.noreply.github.com>
2025-02-04 16:44:35 -0500

    hi

--- Movies-Store/urls.py
+++ Movies-Store/urls.py
@@ -20,4 +20,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('users/', include('users.urls')),
+    path('movies/', include('django.contrib.auth.urls')),
 ]
