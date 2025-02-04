f19f960b96e12a066d13152b3cbf517ef7f8917f
EllaDrag <148929204+EllaDrag@users.noreply.github.com>
2025-02-04 16:02:29 -0500

    login/logout and admin
    created users directory where login and logout are implemented. also implemented admin

--- Movies-Store/urls.py
+++ Movies-Store/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('users/', include('users.urls')),
+    path('users/', include('django.contrib.auth.urls')),
 ]
+
+
