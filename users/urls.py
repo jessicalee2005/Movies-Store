203f02c978f86f94ff7620ab4b2f6c693dbede52
Maddie White <69733729+MuffinHead12@users.noreply.github.com>
2025-02-16 20:00:47 -0500

    reset password (ella)

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('signup', views.signup, name='users.signup'),
+    path('signup', views.register, name='users.signup'),
     path('login/', views.login, name='users.login'),
     path('logout/', views.logout, name='users.logout'),
     path('orders/', views.orders, name='accounts.orders'),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="users/reset_password.html"), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="users/reset_password_sent.html"), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="users/reset_password_form.html"), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="users/reset_password_complete.html"), name='password_reset_complete'),
 ]
